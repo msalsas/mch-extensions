ca353c32a2f991585c30a7e175f8a97c82ba378b
Manolo Salsas <manolez@gmail.com>
2022-12-28 12:54:02 +0100

    Install vuetify

--- src/main.js
+++ src/main.js
@@ -4,10 +4,31 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+// Vuetify
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        }
+    },
+})
+
 import "./assets/main.css";
 
 const app = createApp(App);
 
+app.use(vuetify);
 app.use(createPinia());
 app.use(router);
 
